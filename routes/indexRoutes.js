59d3bda7bbe8b7e75c1c3ee4b7563af80189a135
Cj Alexander <clauvinsalexander@gmail.com>
2024-08-14 08:21:25 -0400

    feat: add ability to view an individual message

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,11 +4,13 @@ const router = express.Router()
 
 const messages = [
     {
+        id: 0,
         text: 'Hi there!',
         user: 'Armando',
         added: new Date()
     },
     {
+        id: 1,
         text: 'Hello World!',
         user: 'Charles',
         added: new Date()
@@ -16,15 +18,22 @@ const messages = [
 ]
 
 router.get('/', (req, res) => {
-    res.render('index', {messages: messages})
+    res.render('index', {messages: messages, hideOpen: false})
 })
 
 router.get('/new', (req, res) => {
     res.render('form')
 })
 
+router.get('/messages/:messageID', (req, res) => {
+    const messageID = req.params.messageID
+    const message = messages[messageID]
+    res.render('message', {message: message, hideOpen: true})
+})
+
 router.post('/new', (req, res) => {
-    messages.push({ text: req.body.userMessage, user: req.body.userName, added: new Date() })
+    messages.push({ id: messages.length, text: req.body.userMessage, user: req.body.userName, added: new Date() })
+    
     // send users back to homepage
     res.redirect('/')
 })
