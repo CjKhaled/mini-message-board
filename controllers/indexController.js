15ba0d173c96c540329eec70f0800c26c74ec22b
Cj Alexander <clauvinsalexander@gmail.com>
2024-08-14 09:16:06 -0400

    refactor: follow MVC pattern

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,30 @@
+const messages = [
+    {
+        id: 0,
+        text: 'Hi there!',
+        user: 'Armando',
+        added: new Date()
+    },
+    {
+        id: 1,
+        text: 'Hello World!',
+        user: 'Charles',
+        added: new Date()
+    }
+]
 
 module.exports = {
-    getMessages: (req, res) => res.render('index', {messages: messages})
+    getMessages: (req, res) => res.render('index', {messages: messages, hideOpen: false}),
+    getForm: (req, res) => res.render('form'),
+    getSingleMessage: (req, res) => {
+        const messageID = req.params.messageID
+        const message = messages[messageID]
+        res.render('message', {message: message, hideOpen: true})
+    },
+    postNewMessage: (req, res) => {
+        messages.push({ id: messages.length, text: req.body.userMessage, user: req.body.userName, added: new Date() })
+    
+        // send users back to homepage
+        res.redirect('/')
+    }
 }
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,41 +2,13 @@ const express = require('express')
 const indexController = require('../controllers/indexController')
 const router = express.Router()
 
-const messages = [
-    {
-        id: 0,
-        text: 'Hi there!',
-        user: 'Armando',
-        added: new Date()
-    },
-    {
-        id: 1,
-        text: 'Hello World!',
-        user: 'Charles',
-        added: new Date()
-    }
-]
+router.get('/', indexController.getMessages)
 
-router.get('/', (req, res) => {
-    res.render('index', {messages: messages, hideOpen: false})
-})
+router.get('/new', indexController.getForm)
 
-router.get('/new', (req, res) => {
-    res.render('form')
-})
+router.get('/messages/:messageID', indexController.getSingleMessage)
 
-router.get('/messages/:messageID', (req, res) => {
-    const messageID = req.params.messageID
-    const message = messages[messageID]
-    res.render('message', {message: message, hideOpen: true})
-})
-
-router.post('/new', (req, res) => {
-    messages.push({ id: messages.length, text: req.body.userMessage, user: req.body.userName, added: new Date() })
-    
-    // send users back to homepage
-    res.redirect('/')
-})
+router.post('/new', indexController.postNewMessage)
 
 
 module.exports = router 
